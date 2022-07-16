31b0ee121a76a539ce5b8ce63b38640b05e6cc4c
Anton Kuznetsov <antakuznet@gmail.com>
2022-07-15 21:58:54 -0700

    Added more steps to onboarding process

--- src/App.js
+++ src/App.js
@@ -1,53 +1,82 @@
-import { useState } from 'react';
-import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import { ControlledOnboardingFlow } from "./ControlledOnboardingFlow";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+import { useState } from "react";
 
 const StepOne = ({ goToNext }) => (
-	<>
-	<h1>Step 1</h1>
-	<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
-	</>
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
+  </>
 );
 const StepTwo = ({ goToNext }) => (
-	<>
-	<h1>Step 2</h1>
-	<button onClick={() => goToNext({ age: 50 })}>Next</button>
-	</>
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({ age: 50 })}>Next</button>
+  </>
 );
 const StepThree = ({ goToNext }) => (
-	<>
-	<h1>Step 3</h1>
-	<p>Congratulations! You qualify for our senior discount</p>
-	<button onClick={() => goToNext({})}>Next</button>
-	</>
+  <>
+    <h1>Step 3</h1>
+    <p>Congratulations! You qualify for our senior discount</p>
+    <button onClick={() => goToNext({})}>Next</button>
+  </>
 );
 const StepFour = ({ goToNext }) => (
-	<>
-	<h1>Step 4</h1>
-	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
-	</>
+  <>
+    <h1>Step 4</h1>
+    <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
+  </>
+);
+
+const StepFive = ({ goToNext }) => (
+  <>
+    <h1>Step 5</h1>
+    <button onClick={() => goToNext({ size: "L" })}>Next</button>
+  </>
+);
+
+const StepSix = ({ goToNext }) => (
+  <>
+    <h1>Step 6</h1>
+    <button onClick={() => goToNext({ height: "6.2" })}></button>
+  </>
+);
+const StepSeven = ({ goToNext }) => (
+  <>
+    <h1>Step 7</h1>
+    <button onClick={() => goToNext({ weight: "190" })}></button>
+  </>
+);
+const StepEight = ({ goToNext }) => (
+  <>
+    <h1>Step 8</h1>
+    <button onClick={() => goToNext({ status: "maried" })}></button>
+  </>
+);
+
+const StepNine = ({ goToNext }) => (
+  <>
+    <h1>Step 9</h1>
+    <button onClick={() => goToNext({ salary: "200" })}></button>
+  </>
 );
 
 function App() {
-	const [onboardingData, setOnboardingData] = useState({});
-	const [currentIndex, setCurrentIndex] = useState(0);	
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	const onNext = stepData => {
-		setOnboardingData({ ...onboardingData, ...stepData });
-		setCurrentIndex(currentIndex + 1);
-	}
+  const onNext = (stepData) => {
+    setOnboardingData({ ...onboardingData, ...stepData });
+    setCurrentIndex(currentIndex + 1);
+  };
 
-	return (
-		<ControlledOnboardingFlow
-			currentIndex={currentIndex}
-			onNext={onNext}
-		>
-			<StepOne />
-			<StepTwo />
-			{onboardingData.age >= 62 && <StepThree />}
-			<StepFour />
-		</ControlledOnboardingFlow>
-	);
+  return (
+    <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
+      <StepOne />
+      <StepTwo />
+      {onboardingData.age >= 62 && <StepThree />}
+      <StepFour />
+    </ControlledOnboardingFlow>
+  );
 }
-
 export default App;
